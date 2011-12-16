10a9481d0aaf1f29681493ce6f0806ef80fc6796
Guto Maia <guto@guto.net>
2011-12-16 00:01:31 -0200

    L4D2 testcase completed

--- src/steamer/Stats.php
+++ src/steamer/Stats.php
@@ -8,7 +8,13 @@ class Stats {
         return array(
             "visibilityState"=>3,
             "gameFriendlyName"=>"L4D2",
-            "gameName"=>"Left 4 Dead 2"
+            "gameName"=>"Left 4 Dead 2",
+            "gameLink"=>"http://store.steampowered.com/app/550",
+            "gameIcon"=>"http://media.steampowered.com/steamcommunity/public/images/apps/550/7d5a243f9500d2f8467312822f8af2a2928777ed.jpg",
+            "gameLogo"=>"http://media.steampowered.com/steamcommunity/public/images/apps/550/205863cc21e751a576d6fff851984b3170684142.jpg",
+            "gameLogoSmall"=>"http://media.steampowered.com/steamcommunity/public/images/apps/550/205863cc21e751a576d6fff851984b3170684142_thumb.jpg",
+            "steamID64"=>76561197985077150,
+            "playerCustomURL"=>"gutomaia"
         );
     }
 
--- tests/L4D2StatsTest.php
+++ tests/L4D2StatsTest.php
@@ -33,4 +33,28 @@ class L4D2StatsTest extends PHPUnit_Framework_TestCase {
     public function testL4D2getGameName(){
         $this->assertEquals("Left 4 Dead 2", $this->stats["gameName"]);
     }
+
+    public function testL4D2getGameLink(){
+        $this->assertEquals('http://store.steampowered.com/app/550', $this->stats['gameLink']);
+    }
+
+    public function testL4D2getGameIcon(){
+        $this->assertEquals('http://media.steampowered.com/steamcommunity/public/images/apps/550/7d5a243f9500d2f8467312822f8af2a2928777ed.jpg', $this->stats['gameIcon']);
+    }
+
+    public function testL4D2getGameLogo(){
+        $this->assertEquals('http://media.steampowered.com/steamcommunity/public/images/apps/550/205863cc21e751a576d6fff851984b3170684142.jpg', $this->stats['gameLogo']);
+    }
+
+    public function testL4D2getGameLogoSmall(){
+        $this->assertEquals('http://media.steampowered.com/steamcommunity/public/images/apps/550/205863cc21e751a576d6fff851984b3170684142_thumb.jpg', $this->stats['gameLogoSmall']);
+    }
+
+    public function testL4D2getPlayerSteamID64(){
+        $this->assertEquals(76561197985077150, $this->stats['steamID64']);
+    }
+
+    public function testL4D2getPlayerCustomUrl(){
+        $this->assertEquals('gutomaia', $this->stats['playerCustomURL']);
+    }
 }
