30980317a51550f035cfff07c4bff8e37f6713f3
Guto Maia <guto@guto.net>
2011-12-16 01:35:37 -0200

    completed Portal2StatsTest

--- src/steamer/Stats.php
+++ src/steamer/Stats.php
@@ -23,6 +23,13 @@ class Stats {
             $stats['gameIcon'] = 'http://media.steampowered.com/steamcommunity/public/images/apps/440/e3f595a92552da3d664ad00277fad2107345f743.jpg';
             $stats['gameLogo'] = 'http://media.steampowered.com/steamcommunity/public/images/apps/440/07385eb55b5ba974aebbe74d3c99626bda7920b8.jpg';
             $stats['gameLogoSmall'] = 'http://media.steampowered.com/steamcommunity/public/images/apps/440/07385eb55b5ba974aebbe74d3c99626bda7920b8_thumb.jpg';
+        } else if ($game == 'portal2'){
+            $stats['gameFriendlyName'] = 'Portal2';
+            $stats['gameName'] = 'Portal 2';
+            $stats['gameLink'] = 'http://store.steampowered.com/app/620';
+            $stats['gameIcon'] = 'http://media.steampowered.com/steamcommunity/public/images/apps/620/2e478fc6874d06ae5baf0d147f6f21203291aa02.jpg';
+            $stats['gameLogo'] = 'http://media.steampowered.com/steamcommunity/public/images/apps/620/d2a1119ddc202fab81d9b87048f495cbd6377502.jpg';
+            $stats['gameLogoSmall'] = 'http://media.steampowered.com/steamcommunity/public/images/apps/620/d2a1119ddc202fab81d9b87048f495cbd6377502_thumb.jpg';
         }
         return $stats;
     }
--- tests/Portal2StatsTest.php
+++ tests/Portal2StatsTest.php
@@ -15,6 +15,30 @@ class Portal2StatsTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals(3, $this->stats["visibilityState"]);
     }
 
+    public function testPortal2getGameFriendlyName(){
+        $this->assertEquals('Portal2', $this->stats['gameFriendlyName']);
+    }
+
+    public function testPortal2getGameName(){
+        $this->assertEquals('Portal 2', $this->stats['gameName']);
+    }
+
+    public function testPortal2getGameLink(){
+        $this->assertEquals('http://store.steampowered.com/app/620', $this->stats['gameLink']);
+    }
+
+    public function testPortal2getGameIcon(){
+        $this->assertEquals('http://media.steampowered.com/steamcommunity/public/images/apps/620/2e478fc6874d06ae5baf0d147f6f21203291aa02.jpg', $this->stats['gameIcon']);
+    }
+
+    public function testPortal2getGameLogo(){
+        $this->assertEquals('http://media.steampowered.com/steamcommunity/public/images/apps/620/d2a1119ddc202fab81d9b87048f495cbd6377502.jpg', $this->stats['gameLogo']);
+    }
+
+    public function testPortal2getGameLogoSmall(){
+        $this->assertEquals('http://media.steampowered.com/steamcommunity/public/images/apps/620/d2a1119ddc202fab81d9b87048f495cbd6377502_thumb.jpg', $this->stats['gameLogoSmall']);
+    }
+
     public function testPortal2getSteamID64(){
         $this->assertEquals(76561197985077150, $this->stats['steamID64']);
     }
